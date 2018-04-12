4dc80bac3c773cb3313e5162ea0c1f500fc37f22
OkyWiliarso <okywiliarso@gmail.com>
2018-04-12 17:41:05 +0700

    done

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 const fs = require('fs');
-var sleep = require('sleep');
+// var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
+function match_data(parent_file, children_file, callback) {
   // your code here...
+  fs.readFile(parent_file, 'utf-8', (err, data) => {
+    // sleep.sleep(5);
+    let parents = JSON.parse(data)
+    let childrens = JSON.parse(fs.readFileSync(children_file, 'utf-8'))
+  
+    for(var i = 0; i < parents.length; i++){
+      let arrChildren = [];
+      for(var j = 0; j < childrens.length; j++){
+        if(parents[i].last_name === childrens[j].family){
+          arrChildren.push(childrens[j].full_name);
+        }
+        parents[i].childrens = arrChildren;
+      }
+    }
+    callback(parents);
+  })
 }
 
-match_data('./parents.json', './childrens.json')
+match_data('./parents.json', './childrens.json', parents => {
+  console.log(parents)
+})
 console.log("Notification : Data sedang diproses !");
