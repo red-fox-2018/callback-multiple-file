d5d84b0fc63761d5e36565f1589aa2f587e274d5
anugrah94 <anugrahrezkifauzinasution@gmail.com>
2018-04-12 17:54:02 +0700

    selesai anchor callback multiple file

--- index.js
+++ index.js
@@ -1,9 +1,37 @@
 const fs = require('fs');
 var sleep = require('sleep');
 
-function match_data(parent_file, children_file) {
-  // your code here...
+
+function getData(fileName, callback){
+  fs.readFile(fileName,'utf8',function(err,data){
+    var obj_data = JSON.parse(data)     
+    callback(obj_data)
+  })
 }
 
-match_data('./parents.json', './childrens.json')
+function match_data(parent_file, children_file, callback) {
+    getData('parents.json', function(parents) {
+      for(let obj of parents){
+        obj.childrens = []
+      }
+      getData('childrens.json', function(childrens){
+        for(var obj1 of parents){
+          for(var obj2 of childrens){
+            if(obj1.last_name == obj2.family){
+              obj1.childrens.push(obj2.full_name)
+            }
+          }
+        }
+        callback(parents)
+      })
+    })
+}
+
+// getData(function(parents, childrens){
+  match_data('./parents.json', './childrens.json', function(parents,childrens){
+    sleep.sleep(5)
+    console.log(parents)
+  })
+// })
 console.log("Notification : Data sedang diproses !");
+
