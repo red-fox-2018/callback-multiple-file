1f0e1fa5e7bca3b6d22ac1c4d2547452bbe62e4e
ariefardi <forzaariefpyon@gmail.com>
2018-04-12 18:28:14 +0700

    need revisi

--- index.js
+++ index.js
@@ -1,8 +1,32 @@
 const fs = require('fs');
-var sleep = require('sleep');
-
+var sleep = require('sleep')
+function getData(filename,cb) {
+fs.readFile(filename,'utf-8',(err,data_str))
+}
 function match_data(parent_file, children_file) {
   // your code here...
+  fs.readFile(parent_file,'utf-8',(err,dataParent) =>{
+    if (err) {
+      console.log(err);
+    }
+    sleep.sleep(1)
+    dataParent = JSON.parse(dataParent)
+    for(let i=0;i<dataParent.length;i++){
+      dataParent[i]['childrens'] = []
+    }
+    //console.log(dataParent);
+    fs.readFile(children_file,'utf-8',(err,dataChildren) =>{
+      if (err) {
+        console.log(err);
+      }
+      sleep.sleep(1)
+      dataChildren = JSON.parse(dataChildren)
+      console.log(dataChildren);
+      for(var i=0;i<dataParent.length;i++){
+        for(var j=0;j<dataChildren.length;j++)
+      }
+    })
+  })
 }
 
 match_data('./parents.json', './childrens.json')
