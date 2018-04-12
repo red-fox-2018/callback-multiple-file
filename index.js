67771a5f3b1ce1c867c85edf93bf3f8fac858e3a
ihsanmaulanahamdani <ihsanmaulana5@gmail.com>
2018-04-12 17:27:28 +0700

    cara 2

--- index.js
+++ index.js
@@ -1,14 +1,28 @@
 const fs = require('fs');
 // var sleep = require('sleep');
 
+function readFile(path, callback) {
+  fs.readFile(path, 'utf-8', (err, data) => {
+    callback(JSON.parse(data));
+  });
+}
+
 function match_data(parent_file, children_file, callback) {
   // your code here...
-  fs.readFile(parent_file, 'utf-8', (err, parents) => {
-    fs.readFile(children_file, 'utf-8', (err, childrens) => {
-      callback(JSON.parse(parents), JSON.parse(childrens));
+
+  // Cara 1
+  // fs.readFile(parent_file, 'utf-8', (err, parents) => {
+  //   fs.readFile(children_file, 'utf-8', (err, childrens) => {
+  //     callback(JSON.parse(parents), JSON.parse(childrens));
+  //   });
+  // });
+
+  // Cara 2
+  readFile(parent_file, parents => {
+    readFile(children_file, childrens => {
+      callback(parents, childrens);
     });
   });
-
 }
 
 match_data('./parents.json', './childrens.json', (dataParents, dataChildrens) => {
