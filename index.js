4dc3a6989de9fa5b874b7fc781d157933a181a02
Oky Wiliarso <35809759+OkyWiliarso@users.noreply.github.com>
2018-04-12 17:54:57 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
-// var sleep = require('sleep');
+var sleep = require('sleep');
 
 function match_data(parent_file, children_file, callback) {
   // your code here...
   fs.readFile(parent_file, 'utf-8', (err, data) => {
-    // sleep.sleep(5);
+    sleep.sleep(5);
     let parents = JSON.parse(data)
     let childrens = JSON.parse(fs.readFileSync(children_file, 'utf-8'))
   
