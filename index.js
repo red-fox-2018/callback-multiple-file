e9ea902a9d26903bdcad046298cacc67f6915ddf
Arief Ardi <34484201+ariefardi@users.noreply.github.com>
2018-04-16 00:01:41 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,33 +1,34 @@
 const fs = require('fs');
-var sleep = require('sleep')
-function getData(filename,cb) {
-fs.readFile(filename,'utf-8',(err,data_str))
+var sleep = require('sleep');
+
+function dataJson (file,cb) {
+	// body... 
+	//sleep.sleep(1)
+	fs.readFile(file,'utf-8',(err,dataFile)=> {
+		if (err) {
+			console.log(err)
+		}
+		dataFile = JSON.parse(dataFile)
+		cb(dataFile)
+	})
 }
 function match_data(parent_file, children_file) {
   // your code here...
-  fs.readFile(parent_file,'utf-8',(err,dataParent) =>{
-    if (err) {
-      console.log(err);
-    }
-    sleep.sleep(1)
-    dataParent = JSON.parse(dataParent)
-    for(let i=0;i<dataParent.length;i++){
-      dataParent[i]['childrens'] = []
-    }
-    //console.log(dataParent);
-    fs.readFile(children_file,'utf-8',(err,dataChildren) =>{
-      if (err) {
-        console.log(err);
-      }
-      sleep.sleep(1)
-      dataChildren = JSON.parse(dataChildren)
-      console.log(dataChildren);
-      for(var i=0;i<dataParent.length;i++){
-        for(var j=0;j<dataChildren.length;j++)
-      }
-    })
+    dataJson(parent_file,function(parents){
+  	dataJson(children_file, function(childrens){
+  		for(var i=0;i<parents.length;i++){
+  			parents[i].childrens = []
+  			for(var j=0;j<childrens.length;j++){
+  				if (childrens[j].family === parents[i].last_name) {
+  					parents[i].childrens.push(childrens[j].full_name)
+  				}
+  			}
+  		}
+  		console.log(parents)
+  	})
   })
+    //return console.log(fileParents)
 }
-
 match_data('./parents.json', './childrens.json')
 console.log("Notification : Data sedang diproses !");
+console.log('./parents.json', './childrens.json')
